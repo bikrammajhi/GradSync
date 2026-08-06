56011ea771462e0a9a50558aba8bce09f7cd429d
bikrammajhi <iiserkbikram@gmail.com>
2026-08-06 04:15:48 +0000

    fix: resolve runtime errors in utils.py
    - Import fcntl (used by print's inter-process lock, previously missing)
    - Remove broken duplicate set_all_seed shadowing the correct one
      (np.seed does not exist; torch.seed takes no seed arg)

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import builtins
+import fcntl
 import random
 
 import numpy as np
@@ -13,7 +14,34 @@ def set_all_seed(seed):
 
 
 def print(*args, is_print_rank=True, **kwargs):
-    """ 
+    """ Solves multi-process interleaved printing issue. """
     
-    if is_print_rank:
-        builtins.print(*args, **kwargs)
+    if not is_print_rank: return
+    with open(__file__, "r") as fh:
+        fcntl.flock(fh, fcntl.LOCK_EX)
+        try:
+            builtins.print(*args, **kwargs)
+        finally:
+            fcntl.flock(fh, fcntl.LOCK_UN)
+
+def to_readable_format(num, precision=3):
+    num_str = str(num)
+    length = len(num_str)
+    
+    def format_with_precision(main, decimal, suffix):
+        if precision == 0:
+            return f"{main}{suffix}"
+        return f"{main}.{decimal[:precision]}{suffix}"
+    
+    if length > 12: 
+        return format_with_precision(num_str[:-12], num_str[-12:], "T")
+    elif length > 9:
+        return format_with_precision(num_str[:-9], num_str[-9:], "B")
+    elif length > 6:
+        return format_with_precision(num_str[:-6], num_str[-6:], "M")
+    elif length > 3:
+        return format_with_precision(num_str[:-3], num_str[-3:], "K")
+    else: 
+        return num_str
+    
+    
